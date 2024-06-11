6d3d32d17b30a413dc4a410854f9b86156e67654
manuelitoys <44844783+manuelitoys@users.noreply.github.com>
2024-06-11 15:03:44 -0500

    Create subscribe.ps1

---
+++ subscribe.ps1
@@ -0,0 +1,21 @@
+set channel to "youtube.com/channel/UC_LU3p_xh3atdBTlHvgXEtw"
+set url to channel & "?sub_confirmation=1"
+
+tell application "Safari"
+    activate
+    open location url
+end tell
+
+delay 5
+
+tell application "System Events"
+    keystroke (ASCII character 9) using {command down} -- Simula TAB
+    delay 0.5
+    keystroke (ASCII character 9) using {command down} -- Simula TAB nuevamente
+    delay 0.5
+    keystroke return -- Simula ENTER
+end tell
+
+delay 1
+
+tell application "Safari" to close window 1
